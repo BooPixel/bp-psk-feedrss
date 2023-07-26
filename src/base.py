44d7c10fa683eb80a93376b46cbf9d78890d47ea
FernandoCelmer <email@fernandocelmer.com>
2023-07-26 20:46:08 -0300

    ⚙️ FEATURE: Update

--- src/base.py
+++ src/base.py
@@ -89,7 +89,7 @@ class Chrome:
         """Chrome Browser
         """
         driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()),
+            executable_path=ChromeDriverManager(version="114.0.5735.16").install(),
             options=self.options()
         )
         driver.implicitly_wait(10)
